76bb347ba1f880bfb54b4ff8933dbb98aa239cb0
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-25 15:45:46 -0500

    cambio de credenciales firebase
    se cambian las credenciales de ingreso a las creadas con la cuenta de google

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-import {initializeApp} from 'firebase/app'
-import {getStorage} from 'firebase/storage'
-import dotenv from 'dotenv'
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA8vgRF4vbiurQoeZ_fNrv5_7JKTxmcHjE",
-    authDomain: "cargar-imagenes-horizons.firebaseapp.com",
-    projectId: "cargar-imagenes-horizons",
-    storageBucket: "cargar-imagenes-horizons.appspot.com",
-    messagingSenderId: "98256147001",
-    appId: "1:98256147001:web:68b33fe62660dc11dfb3e8"
-  };
+  apiKey: "AIzaSyAxnePsOIrBj1JbVZAJBOaKtaemHeDTf7Y",
+  authDomain: "cargar-imagenes-horizons-88e0f.firebaseapp.com",
+  projectId: "cargar-imagenes-horizons-88e0f",
+  storageBucket: "cargar-imagenes-horizons-88e0f.appspot.com",
+  messagingSenderId: "530735236465",
+  appId: "1:530735236465:web:16a9794b4dab66367fa2d0",
+};
 
-const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig);
 
-export const storage = getStorage(firebaseApp)
+export const storage = getStorage(firebaseApp);
