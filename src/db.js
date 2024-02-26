36f54d6e33821fcabd14201cfd7547c71baadf14
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-26 10:41:34 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://horizons:74182391@cluster0.l0gx2hi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://horizons:74182391@cluster0.l0gx2hi.mongodb.net/register?retryWrites=true&w=majority&appName=Cluster0');
         console.log(">>> Conectado con la base de datos");
     } catch (error) {
         console.log(error);
