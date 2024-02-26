21ec6b12db9d77a1310dc09d4db71f5ad74bc1b8
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-26 16:46:21 -0500

    se quitan las authrequired
    en tasks se quitan los authrequired

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,14 +7,14 @@ import {createTaskSchema} from '../schemas/task.schema.js'
 const router = Router();
 
 
-router.get('/tasks', authRequired, getTasks);
+router.get('/tasks', getTasks);
 
-router.get('/tasks/:id', authRequired, getTask);
+router.get('/tasks/:id', getTask);
 
-router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask);
+router.post('/tasks', validateSchema(createTaskSchema), createTask);
 
-router.delete('/tasks/:id', authRequired, deleteTask);
+router.delete('/tasks/:id', deleteTask);
 
-router.put('/tasks/:id', authRequired, updateTask);
+router.put('/tasks/:id', updateTask);
 
 export default router
