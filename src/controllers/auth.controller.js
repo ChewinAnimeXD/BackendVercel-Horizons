8ab7cface5fbb4218b45254f5f5d4b5ff32b0df1
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 12:54:55 -0500

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -121,7 +121,7 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.cookie("token", "", {
+  res.cookie("tokensito", "", {
     expires: new Date(0),
   });
   return res.sendStatus(200);
