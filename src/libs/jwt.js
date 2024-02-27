b3f3b030b3a6b9bf0f4979919b111c8b923b556e
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-26 19:47:14 -0500

    regreso

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken';
-import { TOKEN_SECRET } from '../config.js';
+import { TOKEN_SECRET } from "../config.js";
 
-export const createAccesToken = (req, res, next) => {
-  const { token } = req.cookies;
-  const specificCookieValue = req.cookies['00001212']; // Cambia 'tu-cookie-especifica' por el nombre de tu cookie específica
+export function createAccesToken(payload) {
+    return new Promise ((resolve, reject) => {
+        jwt.sign(
+            payload,
+            TOKEN_SECRET,
+        {
+            expiresIn: "1d",
+        },
+        (err, token) => {
+            if(err) reject(err)
+            resolve(token)              
+            }
+        );
+    })
+}
 
-  if (!token || specificCookieValue !== '00001212') {
-    return res.status(401).json({ Message: "No token o cookie específica, autorización denegada" });
-  }
-
-  jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Token inválido" });
-
-    req.user = user;
-    next();
-  });
-};
