23db0bc35e38b1ae6525fe706dee824490b1bdac
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 16:16:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,14 +28,6 @@ app.use("/api", authRoutes);
 app.use("/api", coursesRoutes);
 app.use("/api", calificationsRoutes);
 
-app.get("/api/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({ user: req.user.id });
-    res.json(tasks);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-});
 
 app.post(
   "/api/create-user",
@@ -125,6 +117,69 @@ app.put("/api/updateUsers/:id", async (req, res) => {
   } catch (error) {
     console.error("Error al actualizar el usuario:", error);
     res.status(400).json({ message: 'Ocurrió un error al actualizar el usuario', error });
+  }
+});
+
+// Obtener todas las tareas
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    console.log(req.user.id)
+    res.json(tasks);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// Crear una nueva tarea
+app.post("/api/creartasks", async (req, res) => {
+  try {
+    const { title, description, date } = req.body;
+    const newTask = new Task({
+      title,
+      description,
+      date,
+      user: req.user.id,
+    });
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Obtener una tarea específica por ID
+app.get("/api/task/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).populate("user");
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    res.json(task);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Eliminar una tarea por ID
+app.delete("/api/eliminartasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Actualizar una tarea por ID
+app.put("/api/actualizartasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    res.json(task);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
   }
 });
 
