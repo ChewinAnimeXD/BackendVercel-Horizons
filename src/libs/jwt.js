a138ef10faeb7f3ec5df8f22569616743caa4b5e
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 07:58:37 -0500

    Update jwt.js

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -3,14 +3,25 @@ import { TOKEN_SECRET } from "../config.js";
 
 
 export function createAccesToken(payload) {
-    
-    
+    //const token = jwt.sign(payload, process.env.TOKEN_SECRET)
+    //res.cookie("jwt", token)
 
 
-    return new Promise (() => {
+    return new Promise ((resolve, reject) => {
+        jwt.sign(
+            payload,
+            TOKEN_SECRET,
+        {
+            expiresIn: "1d",
+        },
+
         
-        const token = jwt.sign(payload, process.env.TOKEN_SECRET)
-        res.cookie("jwt", token)
+
+        (err, token) => {
+            if(err) reject(err)
+            resolve(token)              
+            }
+        );
     })
 }
 
