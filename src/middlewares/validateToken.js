2cf67ad6836d9e9271627a7233a58391e42c707e
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 18:53:26 -0500

    cambio de authrequired

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,18 +1,21 @@
-import jwt from 'jsonwebtoken'
-import {TOKEN_SECRET} from '../config.js'
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import { TOKEN_SECRET } from '../config.js';
+
+const app = express();
+app.use(cookieParser());
 
 export const authRequired = (req, res, next) => {
-    const { token } = req.cookies.token;
-
-    console.log("Este es el token con reqcookies.token", token)
-    
+    const token = req.cookies.token; // Cambio aquí
+    console.log("este es el token de authrequired", token)
 
     if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if(err) return res.status(403).json({ message: "Token invalido"});
 
-        req.user = user
-        next()
-    })
+        req.user = user;
+        next();
+    });
 };
