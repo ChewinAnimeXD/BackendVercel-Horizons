af907dc6dddd47fe42f55b5dbf72ee6ec201ee64
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 19:10:16 -0500

    cambio de cookies

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cookieParser());
 
 export const authRequired = (req, res, next) => {
-    const token = req.Cookies['token']; // Cambio aquí
+    const token = req.cookies['token']; // Cambio aquí
     console.log("este es el token de authrequired", token)
 
     if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
