2b571cbc806637698b930d6c5793b041af63247d
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 20:35:52 -0500

    cambio

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -7,19 +7,15 @@ const app = express();
 app.use(cookieParser());
 
 export const authRequired = (req, res, next) => {
-    const token = req.cookies['token']; // Reemplaza 'nombre_de_la_cookie' con el nombre real de tu cookie
-    console.log(req.cookies);
+    const token = req.cookies; // Cambio aquí
+    console.log(req.cookies)
 
-    req.user = null; // Definir user como null si no hay token o no se valida
+    if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
 
-    if (token) {
-        jwt.verify(token, TOKEN_SECRET, (err, user) => {
-            if (!err) {
-                req.user = user; // Si el token es válido, establecer el usuario en el objeto de solicitud
-            }
-            next(); // Continuar con la ejecución del middleware independientemente de si hay un token o no
-        });
-    } else {
-        next(); // Continuar con la ejecución del middleware si no hay token
-    }
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        if(err) return res.status(403).json({ message: "Token invalido"});
+
+        req.user = user;
+        next();
+    });
 };
