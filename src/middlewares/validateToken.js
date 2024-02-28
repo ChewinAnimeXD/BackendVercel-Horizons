4e433cc0b682ce46875c82f29c9e202d57fe9d64
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 19:25:37 -0500

    otro cambio

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
-import { TOKEN_SECRET } from '../config.js';
-
-const app = express();
-app.use(cookieParser());
-
 export const authRequired = (req, res, next) => {
-    const token = req.cookies['token']; // Cambio aquí
-    console.log("este es el token de authrequired", token)
+    const cookiesString = req.headers.cookie; // Obtener la cadena de cookies de la cabecera
+    if (!cookiesString) return res.status(401).json({ Message: "No hay cookies, autorización denegada" });
 
-    if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
+    // Analizar manualmente la cadena de cookies para encontrar la cookie con nombre 'token'
+    const cookiesArray = cookiesString.split(';').map(cookie => cookie.trim());
+    let token;
+    for (const cookie of cookiesArray) {
+        const [name, value] = cookie.split('=');
+        if (name === 'token') {
+            token = value;
+            break;
+        }
+    }
 
+    if (!token) return res.status(401).json({ Message: "No se encontró la cookie 'token', autorización denegada" });
+
+    // Verificar el token JWT
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if(err) return res.status(403).json({ message: "Token invalido"});
+        if (err) return res.status(403).json({ message: "Token inválido" });
 
         req.user = user;
         next();
