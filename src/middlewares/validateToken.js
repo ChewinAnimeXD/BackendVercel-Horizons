9288ec3b770ef0256550a9616a279990e46e431f
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 19:03:29 -0500

    validate token otra vez

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -7,17 +7,13 @@ const app = express();
 app.use(cookieParser());
 
 export const authRequired = (req, res, next) => {
-    const token = req.cookies.token;
-
-    console.log("Valor de la cookie 'token':", token);
+    const token = req.Cookies.token; // Cambio aquí
+    console.log("este es el token de authrequired", token)
 
     if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if(err) {
-            console.error("Error al verificar el token:", err); // Agrega este registro
-            return res.status(403).json({ message: "Token invalido"});
-        }
+        if(err) return res.status(403).json({ message: "Token invalido"});
 
         req.user = user;
         next();
