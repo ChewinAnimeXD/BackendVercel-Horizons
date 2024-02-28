688605d88193eb985462524219f62f431245fb76
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-27 21:06:39 -0500

    const token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,12 +2,13 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import { TOKEN_SECRET } from '../config.js';
+import { tokensito } from '../controllers/auth.controller.js';
 
 const app = express();
 app.use(cookieParser());
 
 export const authRequired = (req, res, next) => {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGNhNjE0ZjhiN2YyM2RhMmI4YThjZSIsImlhdCI6MTcwOTA4NDE5NywiZXhwIjoxNzA5MTcwNTk3fQ.QXwg0kahkq-bwjCgggBg-yY95FtdJU635ml-POu8f54"; // Cambio aquí
+    const token = tokensito; // Cambio aquí
     console.log(token)
 
     if (!token) return res.status(401).json({ Message: "No token, autorización denegada " });
