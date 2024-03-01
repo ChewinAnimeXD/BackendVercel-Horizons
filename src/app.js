8af63fc8dbb71e1ebe0bd78d7ea5a4dc773ce9f2
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-03-01 18:42:48 -0500

    gfd

--- src/app.js
+++ src/app.js
@@ -121,4 +121,70 @@ app.put("/api/updateUsers/:id", async (req, res) => {
   }
 });
 
+
+/** 
+// Obtener todas las tareas
+app.get("/api/obtenerTasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    console.log("tareas? ",req.user.id)
+    res.json(tasks);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// Crear una nueva tarea
+app.post("/api/creartasks", async (req, res) => {
+  try {
+    const { title, description, date } = req.body;
+    const newTask = new Task({
+      title,
+      description,
+      date,
+      user: req.user.id,
+    });
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Obtener una tarea específica por ID
+app.get("/api/task/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).populate("user");
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    res.json(task);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Eliminar una tarea por ID
+app.delete("/api/eliminartasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+});
+
+// Actualizar una tarea por ID
+app.put("/api/actualizartasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!task) return res.status(404).json({ message: "Tarea no encontrada" });
+    res.json(task);
+  } catch (error) {
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
+  
+})*/;
+
 export default app;
